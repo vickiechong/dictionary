79d43e873ffca8524b7f73d80604c7844f0613bb
vickiechong <81544596+vickiechong@users.noreply.github.com>
2021-06-21 14:14:11 +0800

    Fixes setup code

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p className="App-header">Dictionary</p>
-      <p>
+      <header className="App-header">Dictionary Search</header>
+      <p className="credits">
         Open-sourced by
         <a href="https://github.com/vickiechong/dictionary"> Github </a>, and
         hosted on Netify, coded by Vickie Chong{" "}
