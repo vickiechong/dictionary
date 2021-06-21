3f5621fcb64105d720d188430b447d84196361d0
vickiechong <81544596+vickiechong@users.noreply.github.com>
2021-06-21 14:06:15 +0800

    Fix react setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <p className="App-header">Dictionary</p>
+      <p>
+        Open-sourced by
+        <a href="https://github.com/vickiechong/dictionary"> Github </a>, and
+        hosted on Netify, coded by Vickie Chong{" "}
+      </p>
     </div>
   );
 }
-
-export default App;
